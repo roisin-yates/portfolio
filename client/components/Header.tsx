e6fb8f60ed31acea71d4a51b4c0c1330bc5edfd3
Roisin <113397527+roisin-yates@users.noreply.github.com>
2023-04-28 13:40:21 +1200

    fix accessibility errors

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,12 +5,17 @@ const Header = () => {
     <header className="flex w-full justify-between p-10">
       <h1 className="font-GlitchGoblin text-2xl">RY</h1>
       <div className="flex w-48 justify-evenly">
-        <a href="https://github.com/roisin-yates" className="text-4xl">
+        <a
+          href="https://github.com/roisin-yates"
+          className="text-4xl"
+          aria-label="github profile"
+        >
           <FaGithub />
         </a>
         <a
           href="https://www.linkedin.com/in/roisin-yates-89b212218/"
           className="text-4xl"
+          aria-label="linkedin profile"
         >
           <FaLinkedin />
         </a>
--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -3,7 +3,7 @@ const Profile = () => {
     <div className="mx-20 flex justify-between">
       <div className="flex w-1/2 flex-col">
         <div className="mt-auto mb-auto">
-          <p className="font-GlitchGoblin text-7xl">Hi there ,</p>
+          <h2 className="font-GlitchGoblin text-7xl">Hi there ,</h2>
           <h2 className="pl-6 font-GlitchGoblin text-7xl">I&apos;m Roisin</h2>
           <h3 className="inline-block overflow-visible bg-glitch-background bg-cover py-2 px-4 font-Roboto text-6xl font-bold text-slate-200">
             Front End Developer
